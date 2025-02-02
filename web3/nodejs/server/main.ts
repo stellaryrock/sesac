1250c7d9d439f29fd55e6857e084a6e3dcde67f8
d-w-b <alohakii@naver.com>
2025-02-02 13:41:55 +0900

    interface 'Block'

--- web3/nodejs/server/main.ts
+++ web3/nodejs/server/main.ts
@@ -3,10 +3,11 @@
 //p.219
 
 interface Block{
-    index: number;
-    timestamp : number;
-    data: string;
-    nonce: number;
-    hash: string;
-    previousBlockHash : string;
+    index: number;              // 순차적 블록 번호
+    timestamp : number;         // 새 블록이 블록체인에 추가된 날짜와 시간
+    data: string;               // 한 번 이상의 거래에 대한 데이터
+    nonce: number;              // 채굴자들이 알아내야 할 숫자
+    hash: string;               // 이 블록의 해시
+    previousBlockHash : string; // 이전 블록의 해시값
 }
+
