ff3b956410e08f17aff881d6e4187ea4cb3947dc
stellaryrock <alohakii@naver.com>
2025-05-18 21:26:53 +0900

    float.js

--- kc5/js/float.js
+++ kc5/js/float.js
@@ -1,11 +1,8 @@
-let number = 123.4;
-
-function add(num1, num2){
-  // num1 or num2 가 string 형태의 숫자여도 잘 되는 이유..?
+function add(num1, num2, precision = 1){  
   if( num1 === null || isNaN(num1) ) return NaN;
   if( num2 === null || isNaN(num2) ) return NaN;
 
-  function makeFloat(num, precision = 1){
+  function makeFloat(num, precision){
     let adjustedNum = Math.trunc(num * ( 10 ** precision )); 
 
     return {
@@ -20,12 +17,24 @@ function add(num1, num2){
     }
   }
 
-  let float1 = makeFloat(num1);
-  let float2 = makeFloat(num2);
+  let float1 = makeFloat(num1, precision);
+  let float2 = makeFloat(num2, precision);
 
   return float1.add(float2);
 }
 
-let float3 = add( 1.234, 234.2 ) // 235.4;
-let float4 = add ( '123', 1.23 ) // 124.2;
-console.log(float3, float4);
+let test = [
+  add( 1.234, 234.2, 2 ),  // 235.4;
+  add( '123', 1.23, 1 ),   // 124.2;
+  add( null, 123.12 )   // NaN
+]
+
+for( t of test )
+{
+  console.log(t);
+}
+
+for( i = 0.1; i <= 1; i = add(i , 0.1) )
+{
+  console.log(i);
+}
