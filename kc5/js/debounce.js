fc00d7e70485a7469b1d9d373e90e12d5b7d54c1
stellaryrock <alohakii@naver.com>
2025-06-05 14:48:38 +0900

    js

--- kc5/js/debounce.js
+++ kc5/js/debounce.js
@@ -1,8 +1,18 @@
 const debounce = ( cb, delay ) => {
     let timer;
+    console.log(timer);
 
-    return (...args) => {
-        if (timer) clearTimeout(timer);
-        timer = setTimeout(cb, delay, ...args);
-    }
+    return (...args) => 
+        {
+            if(timer) clearTimeout(timer) ; 
+            timer = setTimeout(cb, delay, ...args) ;
+        }
 }
+
+const cb = () => console.log('함수 실행됨...');
+
+const act = debounce( cb, 4000 );
+setTimeout(()=>console.log('대략 4000ms 지난 후...'), 4000);
+setTimeout(()=>console.log('대략 7500ms 지난 후...'), 7500);
+act();
+setTimeout(()=>{act()}, 3500);
