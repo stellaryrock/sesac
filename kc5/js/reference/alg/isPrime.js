0b17db041f4f07df570d1f2dcb034de2f04721be
stellaryrock <alohakii@naver.com>
2025-06-09 00:47:28 +0900

    js

--- kc5/js/reference/alg/isPrime.js
+++ kc5/js/reference/alg/isPrime.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 // step 1
 
-function isPrime(n) {
+function _isPrime(n) {
     if( n === 2 || n === 3) return true;
 
     for(let i=2; i<n ; i++){
@@ -13,6 +13,19 @@ function isPrime(n) {
 }
 
 //
+
+function isPrime(n){
+    if( n <= 1 ) return false;
+    if( n <= 3 ) return true;
+    
+    if( n % 2 === 0 || n % 3 === 0) return false;
+
+    for(let i = 5; i*i <= n; i += 6 ){
+        if( n % i === 0 || n % (i+2) === 0 ) return false; 
+    }
+
+    return true;
+}
 
 assert.deepStrictEqual(isPrime(11) , true);
 assert.deepStrictEqual(isPrime(2), true);
