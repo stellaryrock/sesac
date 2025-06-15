c4b5635faf846d1b5f03b6fa3891f59cdbc20d71
stellaryrock <alohakii@naver.com>
2025-06-15 21:46:11 +0900

    js

--- kc5/js/fibo.js
+++ kc5/js/fibo.js
@@ -1,6 +1,6 @@
-// 피보나치 수열 구하기
+// ?��보나�? ?��?�� 구하�?
 
-// 1. Loop 를 통해 구현
+// 1. Loop �? ?��?�� 구현
 function fiboLoop(n){
   let [prev, next] = [0 , 1];
 
@@ -10,32 +10,32 @@ function fiboLoop(n){
 
   return prev;
 }
-console.log('1. Loop 를 통해 구현');
+
+console.log('1. Loop �? ?��?�� 구현');
 for( let i = 1; i< 10; i++)
   console.log(fiboLoop(i));
 console.log('--------------------------------------------');
 
-// 2. 순수 재귀를 통해 구현
+// 2. ?��?�� ?���?�? ?��?�� 구현
 function fiboRecursive(n){
   if(n <= 2) return n-1;
   
   return fiboRecursive(n-2) + fiboRecursive(n-1);
 }
 
-console.log('2. 순수 재귀를 통해 구현');
+console.log('2. ?��?�� ?���?�? ?��?�� 구현');
 for( let i = 1; i< 10; i++)
   console.log(fiboLoop(i));
 console.log('--------------------------------------------');
 
-// 3. memoization 을 활용하여 구현
+// 3. memoization ?�� ?��?��?��?�� 구현
 const memoizedFn = (fn) => {
   let table = {};
 
   return (n) => table[n] || (table[n] = fn(n));
 }
 
-console.log('3. memoization 을 활용하여 구현');
+console.log('3. memoization ?�� ?��?��?��?�� 구현');
 for( let i = 1; i< 10; i++)
   console.log(memoizedFn(fiboRecursive)(i));
 console.log('--------------------------------------------');
-
---
+++ kc5/js/reference/README.md
@@ -0,0 +1,3 @@
+# 货较 陛玫 5扁 己龋丛 内靛
+
+Javascript code from Indiflex. https://github.com/indiflex/
