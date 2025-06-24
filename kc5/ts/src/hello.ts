1cec1f8767dff41eae4a3c55d01ba27fc90b094e
stellaryrock <alohakii@naver.com>
2025-06-24 20:04:05 +0900

    config

--- kc5/ts/src/hello.ts
+++ kc5/ts/src/hello.ts
@@ -1,10 +1,10 @@
 const outStr : string = "hello typescript!"
 
-console.log(outStr);
+const myName : string = "kjs";
 
 const PRIMITIVE : Array<string> = ['number', 'object', 'undefined', 'bigint', 'null', 'Symbol', 'string']
 
-const arr = [1, 123n, {id:3}, Symbol('Hi'), null, undefined, 'hi']
+const arr = [1, {id:3}, Symbol('Hi'), null, undefined, 'hi']
 
 arr.forEach( (element, i) => console.log(typeof element) );
 
