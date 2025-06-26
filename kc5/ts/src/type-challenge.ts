c5b42af32d1022104a683af7310acc212db440a3
stellaryrock <alohakii@naver.com>
2025-06-26 10:40:33 +0900

    ts

--- kc5/ts/src/type-challenge.ts
+++ kc5/ts/src/type-challenge.ts
@@ -18,21 +18,3 @@ type MyPick<T, K extends keyof T> = {
     [key in K] : T[key]
 }
 
-
-
-//2. T의 모든 프로퍼티를 읽기 전용(재할당 불가)으로 바꾸는 내장 제네릭 Readonly<T>를 이를 사용하지 않고 구현하세요.
-
-interface Todo {
-  title: string
-  description: string
-}
-
-const todo: MyReadonly<Todo> = {
-  title: "Hey",
-  description: "foobar"
-}
-
-todo.title = "Hello" // Error: cannot reassign a readonly property
-todo.description = "barFoo" // Error: cannot reassign a readonly property
-
-
