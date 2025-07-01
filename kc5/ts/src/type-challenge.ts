062735f3afb72665fe28c1c2fe9202c1a7303f3c
stellaryrock <alohakii@naver.com>
2025-07-01 16:27:06 +0900

    ts

--- kc5/ts/src/type-challenge.ts
+++ kc5/ts/src/type-challenge.ts
@@ -1,5 +1,4 @@
 //1. T에서 K 프로퍼티만 선택해 새로운 오브젝트 타입을 만드는 내장 제네릭  Pick<T,K> 을 이를 사용하지 않고 구현하세요.
-
 type Person = {
     name : string,
     age : number,
@@ -24,7 +23,6 @@ interface Todo {
   title: string
   description: string
 }
-
 
 type MyReadonly<T> = {
   readonly [k in keyof T] : T[k] 
