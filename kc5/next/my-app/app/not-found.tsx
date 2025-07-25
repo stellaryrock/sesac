4fdb5f512d268b2da981f3dbd2ea2d204924529a
stellaryrock <alohakii@naver.com>
2025-07-25 15:57:09 +0900

    next

---
+++ kc5/next/my-app/app/not-found.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function NotFound(){
+  const pathName = usePathname();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-2xl">
+          {pathName} 페이지를 찾을 수 없습니다! 
+        </h1>
+        <Button onClick={() => router.back()} variant={'outline'}>
+          뒤로가기
+        </Button>
+      </div>
+    </>
+  )
+}
---
+++ kc5/next/my-app/lib/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export const {
+  handlers: {GET, POST},
+  auth,
+  signIn,
+  signOut
+} = NextAuth({
+  providers: [Google({
+    clientId:process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  })],
+  secret: process.env.AUTH_SECRET as string
+});
