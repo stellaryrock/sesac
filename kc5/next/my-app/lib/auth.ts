9c5e8f5a0f5ba08fc0bcb488e848dd8cd374745c
stellaryrock <alohakii@naver.com>
2025-07-25 21:45:03 +0900

    next

--- kc5/next/my-app/lib/auth.ts
+++ kc5/next/my-app/lib/auth.ts
@@ -1,16 +1,18 @@
-import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 export const {
-  handlers: {GET, POST},
+  handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
-  providers: [Google({
-    clientId:process.env.GOOGLE_CLIENT_ID as string,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  })],
-  secret: process.env.AUTH_SECRET as string
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET as string,
 });
